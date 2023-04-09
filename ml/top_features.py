ff75b5e446374d94fc3b6138d4a1c1f75c9a4e2a
darkllen <yankiniigor@gmail.com>
2023-04-09 11:03:55 +0300

    table print for top features

--- ml/top_features.py
+++ ml/top_features.py
@@ -1,23 +1,27 @@
 import pickle
 from train_model import prepare_data
-
+from tabulate import tabulate
 
 def get_top_features(model_filepath, colums):
     model = pickle.load(open(model_filepath, 'rb'))
-    print(model_filepath)
+    print("Logistic regression")
     named_features = list(zip(colums, model.coef_[0]))
     sorted_features = sorted(named_features, key=lambda x: x[1], reverse=True)
-    for column, coef in sorted_features[:20]:
-        print(column, coef)
+    table = []
+    for ind, (column, coef) in enumerate(sorted_features[:20]):
+        table.append([ind+1, column, coef])
+    print(tabulate(table))
 
 
 def get_top_features_tree(model_filepath, colums):
     model = pickle.load(open(model_filepath, 'rb'))
-    print(model_filepath)
+    print("Decision tree")
     named_features = list(zip(colums, model.feature_importances_))
     sorted_features = sorted(named_features, key=lambda x: x[1], reverse=True)
-    for column, coef in sorted_features[:20]:
-        print(column, coef)
+    table = []
+    for ind, (column, coef) in enumerate(sorted_features[:20]):
+        table.append([ind+1, column, coef])
+    print(tabulate(table))
 
 
 if __name__ == '__main__':
