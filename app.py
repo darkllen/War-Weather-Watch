5ae8b833a0ca8adbea2ed82d8e33b036fd81ab65
Vladyslav Vereta <vlvereta.92@gmail.com>
2023-04-19 23:11:57 +0300

    Adds main endpoints and basic index page.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, redirect
 
 sys.path.append('./sources')
 from weatherAPI import get_weather
@@ -16,8 +16,41 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+API_KEY=os.getenv('API_KEY')
+
+# Make sure API key is set
+if API_KEY == None:
+  raise RuntimeError("API_KEY not set!")
+
+# Temporary var for distinguishing fresh/not fresh predictions in the file or db
+is_fresh = False
+
 @app.route("/")
 def index():
-  default_weather = get_weather()
-  return render_template("index.html", default_weather = json.dumps(default_weather))
-  # return render_template("index.html")
+  return render_template("index.html", is_fresh_prediction=is_fresh)
+
+
+# Endpoint to make fresh predictions and store them in the file or db
+@app.route("/api/predictions/refresh", methods=["POST"])
+def api_refresh_predictions():
+  return jsonify(status="OK")
+
+
+# The same as above but for client
+@app.route("/predictions/refresh", methods=["POST"])
+def refresh_predictions():
+  global is_fresh
+  is_fresh = True
+  return redirect("/")
+
+
+# Endpoint to get predictions for all regions
+@app.route("/api/predictions", methods=["POST"])
+def api_get_predictions():
+  return jsonify(status="OK")
+
+
+# The same as above but for client
+@app.route("/predictions", methods=["POST"])
+def get_predictions():
+  return redirect("/")
