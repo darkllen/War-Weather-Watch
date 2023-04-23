1d13133be5b938204451501670505ec2c99a1b20
darkllen <yankiniigor@gmail.com>
2023-04-23 14:15:01 +0300

    upd envs

--- db/get_connection.py
+++ db/get_connection.py
@@ -5,8 +5,8 @@ import os
 @contextmanager
 def get_cursor():
     conn = psycopg2.connect(
-        host=os.environ.get("HOST"),
-        port=os.environ.get("PORT"),
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
         database=os.environ.get("DATABASE"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD")
